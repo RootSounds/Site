8fd32d3c3763145dc8e3b04de37d7edaa57c9b8e
NogPiss <AndrePissuto>
2026-05-14 21:28:56 -0300

    adicao na tela de login já funcionando!!

--- web-data-viz/src/controllers/usuarioController.js
+++ web-data-viz/src/controllers/usuarioController.js
@@ -19,21 +19,9 @@ function autenticar(req, res) {
 
                     if (resultadoAutenticar.length == 1) {
                         console.log(resultadoAutenticar);
+                        console.log(res.json(resultadoAutenticar[0]))
+                        
 
-                        aquarioModel.buscarAquariosPorEmpresa(resultadoAutenticar[0].empresaId)
-                            .then((resultadoAquarios) => {
-                                if (resultadoAquarios.length > 0) {
-                                    res.json({
-                                        id: resultadoAutenticar[0].id,
-                                        email: resultadoAutenticar[0].email,
-                                        nome: resultadoAutenticar[0].nome,
-                                        senha: resultadoAutenticar[0].senha,
-                                        aquarios: resultadoAquarios
-                                    });
-                                } else {
-                                    res.status(204).json({ aquarios: [] });
-                                }
-                            })
                     } else if (resultadoAutenticar.length == 0) {
                         res.status(403).send("Email e/ou senha inválido(s)");
                     } else {
@@ -86,7 +74,7 @@ function cadastrar(req, res) {
     }
 }
 
-function listar(req, res){
+function listar(req, res) {
     usuarioModel.listar().then((resultado) => {
         res.status(200).json(resultado);
     })
