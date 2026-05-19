6f06e409fdac322a0458e35625078beb961049a7
AndrePissuto <AndrePissuto>
2026-05-18 22:11:55 -0300

    o feed esta funcional!!

--- web-data-viz/src/models/midiaModel.js
+++ web-data-viz/src/models/midiaModel.js
@@ -2,7 +2,7 @@ var database = require("../database/config");
 
 function listar() {
   console.log("ta no banco")
-  var instrucaoSql = `SELECT * FROM midia;`;
+  var instrucaoSql = `select * from midia where (album is null and tipo = 'album') or (album is null and tipo = 'musica');`;
 
   return database.executar(instrucaoSql);
 }
