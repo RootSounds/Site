33c1837222b47734f26186dedbfc666ab9d90f15
NogPiss <AndrePissuto>
2026-05-23 13:17:37 -0300

    melhorias no feed popup escrolando

--- web-data-viz/src/models/midiaModel.js
+++ web-data-viz/src/models/midiaModel.js
@@ -8,25 +8,21 @@ function listar() {
 }
 
 function curtir(email, idMidia){
-  console.log(email, idMidia)
   var instrucaoSql = `insert into curtida values(default, default, (select idUsuario from usuario where email = '${email}'), ${idMidia});`
   return database.executar(instrucaoSql);
 } 
 
 function descurtir(email, idMidia){
-  console.log(email, idMidia)
   var instrucaoSql = `delete from curtida where idUsuario = (select idUsuario from usuario where email = '${email}') and idMidia = ${idMidia};`
   return database.executar(instrucaoSql);
 }
 
 function listarCurtidas(idUsuario){
-  console.log(idUsuario)
   var instrucaoSql = `select * from curtida where idUsuario = ${idUsuario};`
   return database.executar(instrucaoSql);
 }
 
 function listarMusicasDoAlbum(idMidia){
-  console.log(idMidia)
   var instrucaoSql = `select * from midia where album = ${idMidia}`
   return database.executar(instrucaoSql);
 }
